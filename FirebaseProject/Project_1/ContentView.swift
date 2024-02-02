8f3b57800dbe92370fd889db498e27544faaba8f
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-02-02 21:58:42 +0600

    welcome

---
+++ FirebaseProject/Project_1/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  FirebaseProject
+//
+//  Created by A.f. Adib on 2/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Color.black
+            
+            RoundedRectangle(cornerRadius: 30, style: .continuous)
+                .foregroundStyle(.linearGradient(colors: [.orange, .red], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 1200, height: 500)
+                .rotationEffect(.degrees(135))
+                .offset(y: -350)
+            
+            VStack(spacing: 20) {
+                Text("Welcome")
+                    .foregroundColor(.white)
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+                    .offset(x: -100, y: -100)
+            }
+            
+        }.ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
