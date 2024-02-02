663006ee30bf8c9eb83effb5091a43df15d9164e
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-02-02 21:30:48 +0600

    Initial Commit

---
+++ FirebaseProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseProject
+//
+//  Created by A.f. Adib on 2/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseProject/FirebaseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseProjectApp.swift
+//  FirebaseProject
+//
+//  Created by A.f. Adib on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
