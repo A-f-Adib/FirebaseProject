b90f18ea629d78e628b13efe5efef2405b2a5c8c
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-02-10 23:18:07 +0600

    Update ContentView.swift

--- Fproject2/Fproject2/ContentView.swift
+++ Fproject2/Fproject2/ContentView.swift
@@ -47,7 +47,7 @@ struct ContentView: View {
                     Text("Project")
                         .fontWeight(.bold)
                         .font(.title)
-                    .foregroundColor(.blue)
+                    
                     
                    
                 }
