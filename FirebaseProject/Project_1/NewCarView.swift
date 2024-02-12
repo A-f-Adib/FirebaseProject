a78ff19cb0fab7f095f0558766336a2b0e92d1ed
A-f-Adib <109586107+A-f-Adib@users.noreply.github.com>
2024-02-12 21:59:17 +0600

    newcar view

---
+++ FirebaseProject/Project_1/NewCarView.swift
@@ -0,0 +1,20 @@
+//
+//  NewCarView.swift
+//  FirebaseProject
+//
+//  Created by A.f. Adib on 2/12/24.
+//
+
+import SwiftUI
+
+struct NewCarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewCarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewCarView()
+    }
+}
